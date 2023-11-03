8c542d01d3dcb327cb1efec1323aacd3d9d17808
zach-morris-txt <zach@developer-morris.com>
2023-11-03 09:58:12 -0400

    Project js logic planning

--- project.js
+++ project.js
@@ -2,6 +2,29 @@
 alert("HELLO")
 
 
+//global variables
+let playerSelection = ""
+
+//function for computer choice rock/paper/scissors
 function getComputerChoice() {
-    
+    //acquire random number
+
+    //if random number between 0 and 3.33 return ROCK
+
+    //else if random number between 3.34 and 6.66 return PAPER
+
+    //else random number between 6.67 and 10 return SCISSORS
+}
+
+function getPlayerChoice(playerSelection) {
+    //acquire player choice of ROCK, PAPER, or SCISSORS  *case insensitive*
+
+}
+
+//function for single round
+function playGame(computerSelection, playerSelection) {
+    //compute winner
+
+    //return string announcing winner
+
 }
