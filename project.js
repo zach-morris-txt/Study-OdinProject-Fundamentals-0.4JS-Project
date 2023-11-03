141cac35469c214e5052ff3cd0ce51831b301d2d
zach-morris-txt <zach@developer-morris.com>
2023-11-03 14:48:11 -0400

    game function initial code and pseduo

--- project.js
+++ project.js
@@ -38,18 +38,66 @@ const playerSelection = getPlayerChoice()
 //function for single round
 function playGame(computerSelection, playerSelection) {
     console.log(`Value is ${computerSelection} and ${playerSelection}`)
-    //calculate winner
     //if computer is rock
-        //win
-        //loss
-        //tie
+    if (computerSelection === "ROCK") {
+        if (playerSelection === "ROCK") {
+            return alert("You tie!")
+        }
+        else if (playerSelection === "PAPER") {
+            return alert("You lose!")
+        }
+        else {
+            return alert("You win!")
+        }
+    }
     //if computer is paper
-        //win
-        //loss
-        //tie
+    else if (computerSelection === "PAPER") {
+        if (playerSelection === "PAPER") {
+            return alert("You tie!")
+        }
+        else if (playerSelection === "ROCK") {
+            return alert("You lose!")
+        }
+        else {
+            return alert("You win!")
+        }
+    }
     //if computer is scissor
-        //win
-        //loss
-        //tie
+    else {
+        if (playerSelection === "SCISSORS") {
+            return alert("You tie!")
+        }
+        else if (playerSelection === "PAPER") {
+            return alert("You lose!")
+        } 
+        //Handle values outside of rock, paper, or scissors
+        else if (playerSelection !== "ROCK") {
+            return alert("YOU CHEATED AND STILL LOST!! Play fair, input a correct value!!!")
+        }
+        else {
+            return alert("You win!")
+        }
+    }
 }
-playGame(computerSelection, playerSelection)
+// playGame(computerSelection, playerSelection)
+
+
+function game(playGame) {
+    //score variable starting at 0
+    let score = 0
+    //loop playRound function
+    for (let i = 0; i < 5; i++) {
+        playGame()
+        if (playGame = "You win!") {
+            score = score++
+        }
+    }
+    //increment score for respective players until game 5
+
+    //console.log the winner every instance
+
+
+    //declare winner using % modulo after fifth game 
+
+}
+game()
